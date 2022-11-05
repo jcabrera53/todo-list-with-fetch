cffd9073e762ec407e5d5d054cb2aa8f61f62174
jcabrera53 <juancabrera53@gmail.com>
2022-11-05 04:10:55 +0000

    lectura de datos de la api y envio de datos con fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 
 
 //include images into your bundle
@@ -6,18 +6,45 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-const [ListaTareas, setListaTareas] = useState(["Walk the dog","Wash my hands","Make the bed","Learn React for the rest of my life"])
+
+useEffect (async()=>{
+	let resp = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jcabrera53")
+	console.log(resp);
+	let todo = await resp.json()
+	setListaTareas(todo);
+},[])
+
+const [texto, setTexto] = useState("") 
+
+async function handle(event){
+	if(event.key == "Enter")
+	{
+		let respuesta = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jcabrera53",{
+			method: 'PUT',
+			headers: {
+			  'Content-Type': 'application/json'
+			},
+			body: JSON.stringify([...ListaTareas,{label: texto, done: false}])
+		  })
+		  let data = await respuesta.json()
+		  setListaTareas([...ListaTareas,{label: texto, done: false}])
+		  setTexto("")
+	}
+}
+
+const [ListaTareas, setListaTareas] = useState([])
 	return (
 		<div className="text-center bg-light">
 			<div className="text-center">TODOS</div>
 			<div className="input-group input-group-lg">
   <span className="input-group-text" id="inputGroup-sizing-lg">Ingrese texto:</span>
-  <input type="text" className="form-control"  />
+  <input type="text" onKeyDown={handle} onChange={(event)=>setTexto(event.target.value)} value={texto} className="form-control"  />
 </div>
 
-  {ListaTareas.map(task => 
-	<div  className="border">
-  <label>{task}</label>
+  {ListaTareas.map((task,index) => 
+	<div  className="border" key={index}>
+  <label>{task.label}</label>
+  <label onClick={()=>console.log(index)}>X</label>
   </div>
 )}
 
